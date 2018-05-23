6306069844c3a82bbba222176250b627c75b3777
leandroif <leandroif@terra.com.br>
2018-05-23 15:04:23 -0300

    Página de producto y ruta lista

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -21,9 +21,11 @@ export class ProductosService {
 
         this.http.get('https://paginaweb-1f164.firebaseio.com/productos_idx.json')
             .subscribe( res => {
-              console.log( res.json());
-                this.cargando = false;
-                this.productos = res.json();
+              //console.log( res.json());
+                //setTimeout(() => {
+                    this.cargando = false;
+                    this.productos = res.json();
+                //},1000);
             });
 
 
