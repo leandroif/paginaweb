7f678ce8f83483ca9acd9e9c7ae669b3d83254fe
leandroif <leandroif@terra.com.br>
2018-05-23 13:27:11 -0300

    servicios de firebase

--- src/app/services/informacion/informacion.service.ts
+++ src/app/services/informacion/informacion.service.ts
@@ -6,17 +6,35 @@ export class InformacionService {
 
   info: any = {};
   cargada: boolean = false;
+  cargada_sobre_nosostros: boolean = false;
+  equipo: any[] = [];
 
   constructor(
       public http: Http
   ) {
+      this.carga_info();
+      this.carga_sobre_nosotros();
+  }
+
+
+  public carga_info() {
       this.http.get('assets/data/info.pagina.json')
           .subscribe(
-          data => {
-            console.log(data.json());
-            this.info = data.json();
-            this.cargada = true;
-          });
+              data => {
+                  //console.log(data.json());
+                  this.info = data.json();
+                  this.cargada = true;
+              });
+  }
+
+  public carga_sobre_nosotros() {
+      this.http.get('https://paginaweb-1f164.firebaseio.com/equipo.json')
+          .subscribe(
+              data => {
+                  console.log(data.json());
+                  this.equipo = data.json();
+                  this.cargada_sobre_nosostros = true;
+              });
   }
 
 }
